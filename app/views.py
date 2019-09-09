413aaa96a71558548d72b443c8ceaa6020f79bf6
Henrique <henrique.braz@acad.pucrs.br>
2019-09-09 17:19:51 -0300

    aula02 30:11 min

--- app/views.py
+++ app/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from .forms import TarefaForm
 
 # Create your views here.
 
 def listar_tarefas(request):
     nome_tarefa = "Assitir a semana Python e Django da Treinaweb"
     return render(request, 'tarefas/listar_tarefas.html',{"nome_tarefa": nome_tarefa })
+
+def cadastrar_tarefa(request):
+    form_tarefa = TarefaForm()
+    return render(request, 'tarefas/form_tarefa.html',{"form_tarefa": form_tarefa})
